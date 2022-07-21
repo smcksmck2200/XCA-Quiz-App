94719dd02af6e217b630032b1502342fd42bd0eb
Sherwin Felton <sherwinfelton36@gmail.com>
2022-07-21 18:32:56 -0400

    capstonequizappxca072022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+
+  const questions = [
+    {
+      questionChoice: "Which is not an OOP programming language?",
+      answerChoice: [
+        { answerText: 'C++', correctAnswer: true },
+        { answerText: 'C', correctAnswer: false },
+        { answerText: 'Python', correctAnswer: true },
+        { answerText: 'Java', correctAnswer: true },
+      ],
+    },
+    {
+      questionChoice: "What is the internet?",
+      answerChoice: [
+        { answerText: 'a net inside of a net', correctAnswer: false },
+        { answerText: 'a musical group', correctAnswer: false },
+        { answerText: 'an interconnected system of public webpages', correctAnswer: false },
+        { answerText: 'a worldwide system of computer networks', correctAnswer: true },
+      ],
+    }
+  ];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleClick = (correctAnswer) => {
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      alert('use have reach the end of the quiz');
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <div className="question-section">
+    
+        <span>Question {currentQuestion + 1}</span>/{questions.length}
+      
+        <div className="question-text">{questions[currentQuestion].questionChoice}</div>
+      </div>
+      
+      <div className='answer-section'>
+        {questions[currentQuestion].answerChoice.map((answerChoice) => (
+          <button onClick={() => handleClick(answerChoice.correctAnswer)}>{answerChoice.answerText}</button>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
